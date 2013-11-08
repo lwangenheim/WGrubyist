3b341151bec02354170ad13a0ab985445865ada7
lwangenheim <lee@busydadwebdesign.com>
2013-11-07 21:28:20 -0500

    thursday night's work

--- ticket.rb
+++ ticket.rb
@@ -23,4 +23,12 @@ puts "This ticket is for: #{ticket.event}, at #{ticket.venue}." +
      "The performer is #{ticket.performer}." +
      "The seat is #{ticket.seat}, " +
      "and it costs $#{"%.2f." % ticket.price}"
-     
+
+print "Information desired: "
+request = gets.chomp
+
+ if ticket.respond_to?(request)
+	 puts ticket.send(request)
+ else
+	 puts "No such information available"
+ end
