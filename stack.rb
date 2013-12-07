4d3f746a904967e584bcd7a125bc6fb04bf64930
lwangenheim <lee@busydadwebdesign.com>
2013-12-07 15:35:56 -0500

    stacks

---
+++ stack.rb
@@ -0,0 +1,23 @@
+require_relative "stacklike"
+class Stack
+
+   include Stacklike
+
+end
+
+s = Stack.new
+
+s.add_to_stack("item one")
+s.add_to_stack("item two")
+s.add_to_stack("item three")
+
+puts "Objects currently on the stack: "
+puts s.stack
+
+taken = s.take_from_stack
+
+puts "removed this object: "
+puts taken
+
+puts "now on stack: "
+puts s.stack
