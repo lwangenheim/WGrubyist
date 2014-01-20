964b65ffb4e8d9c85f4f535ad8599adfba7c2165
lwangenheim <lwangenheim@gmail.com>
2014-01-19 19:10:23 -0500

    footbaw!

--- c2f.rb
+++ c2f.rb
@@ -1,5 +1,16 @@
-celsius = 100
-fahrenheit = (celsius * 9/5) + 32 
-print "the result is: "
-print fahrenheit
-puts "."
+class Temperature 
+def temp(celsius)
+ (celsius * 9/5) + 32 
+end
+celsius = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+puts "Celcius\tFahrenheit"
+for c in celsius
+  puts "c\t#{temp(c)}"
+end
+
+
+#print "the result is: "
+#print fahrenheit
+#puts "."
+end 
+
---
+++ new_ticket.rb
@@ -0,0 +1,28 @@
+class Ticket
+  attr_accessor :venue, :date
+  def initialize(venue, date)
+    self.venue = venue
+    self.date = date
+  end
+
+  def ===(other_ticket)
+    self.venue == other_ticket.venue
+  end
+end
+
+ticket1 = Ticket.new("Town Hall", "07/08/06")
+
+ticket2 = Ticket.new("Conference Center", "07,08,06")
+
+ticket3 = Ticket.new("Town Hall", "08/09/06")
+
+puts "ticket 1 is for an event at: #{ticket1.venue}."
+
+case ticket1
+when ticket2
+  puts "same location as ticket2!"
+when ticket3
+  puts "same location as ticket3!"
+
+else puts "no match"
+end
